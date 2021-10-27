0bcec1db7ecc2b50018b870536e4650f74c6f4ca
faustinogonzalez <abcdfaustino@gmail.com>
2021-10-26 21:47:13 -0500

    cambio

---
+++ aplicacion1/src/aplicacion1/main.java
@@ -0,0 +1,11 @@
+package aplicacion1;
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ aplicacion1/src/module-info.java
@@ -0,0 +1,2 @@
+ 	module aplicacion1 {
+}
